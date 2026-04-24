97b3e49f1d8977c3cc246af90249d07e598d1ec2
Ali Ebrahim <aebrahim@users.noreply.github.com>
2026-04-24 10:46:36 +0000

    Switch to IsolatedAsyncioTestCase.

--- test/test_fastapi.py
+++ test/test_fastapi.py
@@ -28,35 +28,38 @@ class FastapiTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), ["user1", "user2"])
 
-    def test_concurrent_requests(self):
-        async def run():
-            barrier = asyncio.Barrier(75)
 
-            async def make_request(client, path, params, expected):
-                await barrier.wait()
-                response = await client.get(path, params=params)
-                return response.status_code, response.json(), expected
+class FastapiAsyncTest(unittest.IsolatedAsyncioTestCase):
+    async def test_concurrent_requests(self):
+        n = 25
+        barrier = asyncio.Barrier(n * 3)
 
+        async def make_request(client, path, params, expected):
+            await barrier.wait()
+            response = await client.get(path, params=params)
+            return response.status_code, response.json(), expected
+
+        async with fastapi_sample.sample_database_setup(fastapi_sample.app):
             transport = httpx.ASGITransport(app=fastapi_sample.app)
             async with httpx.AsyncClient(
-                transport=transport, base_url="http://test"
+                transport=transport,
+                base_url="http://test",
             ) as client:
                 tasks = []
-                for _ in range(25):
+                for _ in range(n):
                     tasks.append(
                         make_request(client, "/users", {"account_id": 1}, ["user1"])
                     )
-                for _ in range(25):
+                for _ in range(n):
                     tasks.append(
                         make_request(client, "/users", {"account_id": 2}, ["user2"])
                     )
-                for _ in range(25):
+                for _ in range(n):
                     tasks.append(
                         make_request(client, "/all_users", None, ["user1", "user2"])
                     )
-                return await asyncio.gather(*tasks)
+                results = await asyncio.gather(*tasks)
 
-        results = asyncio.run(run())
         for status_code, body, expected in results:
             self.assertEqual(status_code, 200)
             self.assertEqual(body, expected)
